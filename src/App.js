1f4fd0baaa21c3c8800173a23b76fe288c63fe68
Jon Mullingtons <jmulhern85@gmail.com>
2022-03-03 20:51:55 +0000

    Adding links

--- src/App.js
+++ src/App.js
@@ -4,13 +4,28 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <div class="page-bg"></div>
+
+      <div class="animation-wrapper">
+        <div class="particle particle-1"></div>
+        <div class="particle particle-2"></div>
+        <div class="particle particle-3"></div>
+        <div class="particle particle-4"></div>
+      </div>
+
+      <div class="page-wrapper">
+        <h1>CSS Particles</h1>
+      </div>
+
       <header className="App-header">
         <h1>header</h1>
       </header>
       <div id="tileContainer">
         <div className="appTile" id="predictionsTile">
-          <div id="header1">Guess</div>
-          <div id="header2">to Impress</div>
+          <a href="https://predictions.jonmulhern.co.uk">
+            <div id="header1">Guess</div>
+            <div id="header2">to Impress</div>
+          </a>
         </div>
         <div className="appTile">
           <img src="chords.png" alt="Piano Chord Practice Game" />
@@ -20,7 +35,7 @@ function App() {
             alt="Chord practice game coming soon!"
           />
         </div>
-        
+
         <div className="appTile">
           <img src="bird.png" alt="Birdbox Livestream" />
           <img
@@ -30,7 +45,10 @@ function App() {
           />
         </div>
         <div className="appTile">
-          <img src="jmcs_smaller.png" alt="JMCS" />
+          <a href="https://jmcs.jonmulhern.co.uk">
+            ≈
+            <img src="jmcs_smaller.png" alt="JMCS" />
+          </a>
         </div>
       </div>
     </div>
