412c980b5c2dc7762bf2c3bcd1d27210f677e4e2
Jon Mullingtons <jmulhern85@gmail.com>
2022-03-03 15:18:23 +0000

    If it pleases the master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Hello there</header>
+      <body>
+        <div className="appTile"><h1>app 1 here</h1></div>
+        <div className="appTile"><h1>app 2 here</h1></div>
+        <div className="appTile"><h1>app 3 here</h1></div>
+        <div className="appTile"><h1>app 4 here</h1></div>
+      </body>
     </div>
   );
 }
